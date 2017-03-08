f5b2521389f45afeb66d3c2da6f1e2db9802074f
bruno.krebs <krebs.bruno@gmail.com>
2017-03-07 21:21:40 -0300

    Updating auth0 libs versions

--- backend/src/main/java/com/auth0/migration/WebSecurityConfig.java
+++ backend/src/main/java/com/auth0/migration/WebSecurityConfig.java
@@ -1,30 +1,32 @@
 package com.auth0.migration;
 
-import com.auth0.spring.security.api.Auth0SecurityConfig;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.context.annotation.Bean;
+import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableWebSecurity (debug = true)
 @Configuration
-@EnableWebSecurity(debug = true)
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-public class WebSecurityConfig extends Auth0SecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value("${auth0.audience}")
+    private String audience;
 
-    @Bean
-    public Auth0Client auth0Client() {
-        return new Auth0Client(clientId, issuer);
-    }
+    @Value("${auth0.issuer}")
+    private String issuer;
+
+    @Value("${auth0.secret}")
+    private String secret;
 
     @Override
-    protected void authorizeRequests(final HttpSecurity http) throws Exception {
-
+    protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/", "/public").permitAll()
                 .anyRequest().authenticated();
+
+        JwtWebSecurityConfigurer
+                .forHS256(audience, issuer, secret.getBytes())
+                .configure(http);
     }
 }
